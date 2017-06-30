d9e42b8550bcdde8d88230a4de8b006650fed6fe
wueppke <wiebkeheinemann@googlemail.com>
2017-06-30 15:30:32 +0200

    new commit

--- index.php
+++ index.php
@@ -4,22 +4,43 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
-    <head>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
         <title></title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script type="text/javascript">
+        </script>
     </head>
     
     <body>
-        <div class="dropdown">
-  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
-  <span class="caret"></span></button>
-  <ul class="dropdown-menu">
-    <li><a href="#">Hinzufügen</a></li>
-  </ul>
-</div>
+        <div id="element_1">
+            <div class="jumbotron">
+                <h1>Add your Book</h1>
+                <p> Platzhalter</p>
+                <p></p>
+            </div> 
+            <div id="add" class="container site-section">
+                <div class="row">
+                    <div class="col-md-4 item">
+                        <i class="fa fa-user"></i>
+                        <h2>Author</h2>
+                        <p>choose or add an author</p>
+                        <div class="dropdown">
+                            <button class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button">
+                             "Choose"
+                             <span class="caret"></span>
+                            </button>
+                        </div>
+                    </div> 
+                    
+                </div>
+            </div>
+            
+            
+        </div>
+        
     </body>
 </html>
